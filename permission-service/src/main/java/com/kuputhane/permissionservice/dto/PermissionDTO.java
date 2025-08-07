38cb3ba4e4cd6260a0ca6e250eff4973da7e116c
duyguabbasoglu <duygua963@gmail.com>
2025-08-07 23:09:35 +0300

    deploy

--- permission-service/src/main/java/com/kuputhane/permissionservice/dto/PermissionDTO.java
+++ permission-service/src/main/java/com/kuputhane/permissionservice/dto/PermissionDTO.java
@@ -1,5 +1,12 @@
 package com.kuputhane.permissionservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PermissionDTO {
     private Long id;
     private String name;
--- user-service/src/main/java/com/kuputhane/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/kuputhane/userservice/UserServiceApplication.java
@@ -3,8 +3,6 @@ package com.kuputhane.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
